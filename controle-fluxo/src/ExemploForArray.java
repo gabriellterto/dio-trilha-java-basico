3b5078592ba7ac96c7de5057c32b790857ffe610
Gabriel Terto <gabrielterto1002@gmail.com>
2024-09-19 17:16:02 -0300

    Adicionando as classes ExemploBreakContinue, ExemploDoWhile, ExemploFor, ExemploForArray, ExemploWhile para estudas Estruturas de Repetição

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,12 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        // EM ARRAYS O INDICE INICIA EM ZERO
+        String alunos [] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+        // for each
+        for(String aluno : alunos) {
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+
+    }
+}
